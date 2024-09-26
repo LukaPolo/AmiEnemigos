36d63ef3ba59e17957c7fb7e1539f209deb4951d
UrzagasteDarioAlberto <dari01urzagaste@gmail.com>
2024-09-26 00:30:39 -0300

    update
    HUID ACTUALIZADA

--- Assets/Scripts/Menus/irViñeta2.cs
+++ Assets/Scripts/Menus/irViñeta2.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class playGame : MonoBehaviour
+public class irViñeta2 : MonoBehaviour
 {
-   public void EscenaJuego()
+    public void EscenaJuego()
     {
-        SceneManager.LoadScene("MenuSeleccion");
+        SceneManager.LoadScene("Vineta2");
     }
 
-    
+
     public void Salir()
     {
         Application.Quit();
